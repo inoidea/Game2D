040b7a5957406d806f35de34554dfc33231ca829
inoidea <118261243+inoidea@users.noreply.github.com>
2023-04-24 11:46:10 +0300

    root

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+namespace PlatformerMVC
+{
+    public class PlayerController
+    {
+        private AnimationConfig _config;
+        private SpriteAnimController _playerAnimator;
+        private LevelObjectView _playerView;
+
+        private Transform _playerT;
+
+        private float _xAxisInput;
+        private bool _isJump;
+
+        private float _walkSpeed = 3f;
+        private float _animSpeed = 10f;
+        private float _movingTreshold = 0.1f;
+
+        private Vector3 _leftScale = new Vector3(-1, 1, 1);
+        private Vector3 _rightScale = new Vector3(1, 1, 1);
+
+        private bool _isMoving;
+
+        private float _jumpForce = 9f;
+        private float _jumpTreshold = 1f;
+        private float _g = -9.8f;
+        private float _groundLevel = 0.5f;
+        private float _yVelocity;
+
+        public PlayerController(LevelObjectView player) {
+            _config = Resources.Load<AnimationConfig>("SpriteAnimCfg");
+            _playerAnimator = new SpriteAnimController(_config);
+            _playerAnimator.StartAnimation(player._spriteRenderer, AnimState.Run, true, _animSpeed);
+            _playerView = player;
+            _playerT = player._transform;
+        }
+
+        private void MoveTowards() {
+            _playerT.position += Vector3.right * (Time.deltaTime * _walkSpeed * (_xAxisInput < 0  ? -1 : 1));
+            _playerT.localScale = _xAxisInput < 0 ? _leftScale : _rightScale;
+        }
+
+        public bool IsGrounded() { 
+            return _playerT.position.y <= _groundLevel && _yVelocity <= 0;
+        }
+
+        public void Update()
+        {
+            _playerAnimator.Update();
+            _xAxisInput = Input.GetAxis("Horizontal");
+            _isJump = Input.GetAxis("Vertical") > 0;
+
+            _isMoving = Mathf.Abs(_xAxisInput) > _movingTreshold;
+
+            if (_isMoving)
+            {
+                MoveTowards();
+            }
+
+            if (IsGrounded())
+            {
+                _playerAnimator.StartAnimation(_playerView._spriteRenderer, _isMoving ? AnimState.Run : AnimState.Idel, true, _animSpeed);
+
+                if (_isJump && _yVelocity <= 0)
+                {
+                    _yVelocity = _jumpForce;
+                }
+                else if (_yVelocity < 0)
+                {
+                    _yVelocity = 0;
+                    _playerT.position = new Vector3(_playerT.position.x, _groundLevel, _playerT.position.z);
+                }
+            }
+            else
+            {
+                if (Mathf.Abs(_yVelocity) > _jumpTreshold)
+                {
+                    _playerAnimator.StartAnimation(_playerView._spriteRenderer, AnimState.Jump, true, _animSpeed);
+                }
+
+                _yVelocity += _g * Time.deltaTime;
+                _playerT.position += Vector3.up * (Time.deltaTime * _yVelocity);
+            }
+        }
+    }
+}
--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -8,21 +8,16 @@ namespace PlatformerMVC
     public class Game : MonoBehaviour
     {
         [SerializeField] private LevelObjectView _playerView;
-        private AnimationConfig _config;
-        private SpriteAnimController _playerAnimator;
-
-        private float speed = 10f;
+        private PlayerController _playerController;
 
         private void Awake()
         {
-            _config = Resources.Load<AnimationConfig>("SpriteAnimCfg");
-            _playerAnimator = new SpriteAnimController(_config);
-            _playerAnimator.StartAnimation(_playerView._spriteRenderer, AnimState.Run, true, speed);
+            _playerController = new PlayerController(_playerView);
         }
 
         void Update()
         {
-            _playerAnimator.Update();
+            _playerController.Update();
         }
     }
 }
